0d955da8ca43a1f8ef208af8f8663ef106f62711
Kissu1 <ricoposas@gmail.com>
2025-12-12 01:00:02 +0800

    add icon

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" href="/favicon.ico?v=4" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=4" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png?v=4" />
+      <meta name="theme-color" content="#ffffff" />
+    </>
+  )
+}
